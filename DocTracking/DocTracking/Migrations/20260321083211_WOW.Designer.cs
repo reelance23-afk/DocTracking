06839e4e1a9331149286b786a76cc9cc5d89c6a7
Camille Malanum <tobia166@gmail.com>
2026-03-21 16:32:50 +0800

    asdanodi

--- DocTracking/DocTracking/Migrations/20260321083211_WOW.Designer.cs
+++ DocTracking/DocTracking/Migrations/20260321083211_WOW.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DocTracking.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260321010625_DeV")]
-    partial class DeV
+    [Migration("20260321083211_WOW")]
+    partial class WOW
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- DocTracking/DocTracking/Migrations/20260321083211_WOW.cs
+++ DocTracking/DocTracking/Migrations/20260321083211_WOW.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace DocTracking.Migrations
 {
     /// <inheritdoc />
-    public partial class DeV : Migration
+    public partial class WOW : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
