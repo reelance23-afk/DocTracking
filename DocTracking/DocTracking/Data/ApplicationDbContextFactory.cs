a83d881e464f49dd11134a20d8a884037adc199d
Camille Malanum <tobia166@gmail.com>
2026-03-21 16:49:37 +0800

    asd

---
+++ DocTracking/DocTracking/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DocTracking.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseNpgsql("Host=crossover.proxy.rlwy.net;Port=56546;Database=railway;Username=postgres;Password=ntQqnqwFwyYzhOGpdoIUmYfKkAUntPfn;")
+                .Options;
+            return new ApplicationDbContext(options);
+        }
+    }
+}
